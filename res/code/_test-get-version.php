5bee4620eb24abc08b4791b76a44bea0c95151e3
Marc Durdin <marc@keyman.com>
2019-07-21 10:36:28 +1000

    tabs to spaces

--- res/code/_test-get-version.php
+++ res/code/_test-get-version.php
@@ -2,19 +2,19 @@
   require_once('./_get-version.php');
   $keymanVersion = new KeymanVersion();
   $ver = $keymanVersion->getVersion('web', 'stable');
-	echo "Stable: $ver\n";
+  echo "Stable: $ver\n";
   if(empty($ver)) {
     $ver = $keymanVersion->getVersion('web', 'beta');
-		echo "Beta: $ver\n";
+    echo "Beta: $ver\n";
     if(empty($ver)) {
       $ver = $keymanVersion->getVersion('web', 'alpha');
-			echo "Alpha: $ver\n";
+      echo "Alpha: $ver\n";
       if(empty($ver)) {
         echo "console.log('ERROR: KeymanWeb version not found');";
         exit;
       }
     }
   }
-	
-	echo $ver;
+  
+  echo $ver;
 ?>
