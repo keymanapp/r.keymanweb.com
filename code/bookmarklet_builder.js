62923173be85fefbf82f81f51317db3329b94f44
Joshua A. Horton <joshua_horton@sil.org>
2022-11-03 10:08:59 +0700

    chore: CSS -> bookmarklet_ui.css

--- code/bookmarklet_builder.js
+++ code/bookmarklet_builder.js
@@ -5,42 +5,6 @@
   // // Debug / dev version
   // var HOST = 'http://r.keymanweb.com.local';
 
-  function construct_bookmarklet_stylesheet() {
-    var style = document.createElement('style');
-
-    var imgHref = `${HOST}/code/keymanweb-icon-16.png`;
-
-    var styleSpec = `
-    /* Easier to override when less specific */
-    .keyman-bookmarklet {
-      font-size: 11px;
-    }
-
-    .keyman-bookmarklet a {
-      padding: 4px 4px 4px 24px;
-      background: #D3DAED url('${imgHref}') 4px 4px no-repeat scroll;
-      box-shadow: 3px 3px 6px rgba(50,52,56,0.3);
-      text-decoration: none;
-      color: #000000;
-      text-align: left;
-      display: inline-block;
-      height: 16px;
-      overflow: hidden;
-      white-space: nowrap;
-    }
-    `;
-
-    style.id='keyman-bookmarklet-style';
-    style.appendChild(document.createTextNode(styleSpec));
-
-    return style;
-  }
-
-  var bml_style = {
-    element: construct_bookmarklet_stylesheet(),
-    isLoaded: false
-  };
-
   function construct_bookmarklet_code(kbdid, langid) {
     var code = `
 (function(e) {
@@ -79,12 +43,6 @@
       if(typeof _gaq != 'undefined')
         _gaq.push(['_trackEvent', 'Bookmarklet', 'Installing', langid + ',' + kbdname ]);
     };
-
-    // Ensure that the relevant stylesheet gets properly linked to the page!
-    if(!bml_style.isLoaded) {
-      document.head.appendChild(bml_style.element);
-      bml_style.isLoaded = true;
-    }
 
     return bml;
   }
